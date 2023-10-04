ae0b8b87edf95237176f91d9f186bac000fd4359
Ícaro <icaropereirasun@gmail.com>
2023-10-04 19:18:23 -0400

    feat: add function getMediaIdades, getMediaAlturas, getMediaPesos

--- typescript/laboratorio_02/src/model/Turma.ts
+++ typescript/laboratorio_02/src/model/Turma.ts
@@ -26,4 +26,50 @@ export default class Turma implements List {
     removeItem(id: string): void {
         this.list = this.list.filter(item => item.getId() !== id);
     }
+
+    getNumAlunos(): number {
+        return this.list.length;
+    }
+
+    getMediaIdades(): number {
+        let sumIdade: number = 0;
+
+        for (let i = 0; i < this.list.length; i++) {
+            sumIdade += this.list[i].getIdade();
+        }
+        
+        if (this.getNumAlunos() === 0 || sumIdade === 0) {
+            return sumIdade;
+        }
+
+        return sumIdade / this.getNumAlunos();
+    }
+
+    getMediaAlturas(): number {
+        let sumAlturas: number = 0;
+
+        let numAlunos: number = this.getNumAlunos();
+
+        if (numAlunos === 0) 
+        return 0;
+
+        const initialValue: number = 0;
+        sumAlturas = this.list.reduce((accumulator, currentValue) => accumulator + currentValue.getAltura(), initialValue);
+
+        return sumAlturas / numAlunos;
+    }
+
+    getMediaPesos(): number {
+        let sumPesos: number = 0;
+
+        let numAlunos: number = this.getNumAlunos();
+
+        if (numAlunos === 0) 
+        return 0;
+
+        const initialValue: number = 0;
+        sumPesos = this.list.reduce((accumulator, currentValue) => accumulator + currentValue.getPeso(), initialValue);
+
+        return sumPesos / numAlunos;
+    }
 }
