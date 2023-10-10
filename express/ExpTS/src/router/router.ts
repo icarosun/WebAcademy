e8d2fc1a0c9d732103729ed00f459ebd1d7281af
Ícaro <icaropereirasun@gmail.com>
2023-10-09 23:09:19 -0400

    feat: create router ui

--- express/ExpTS/src/router/router.ts
+++ express/ExpTS/src/router/router.ts
@@ -13,5 +13,6 @@ router.get('/hb3', mainController.hb3);
 router.get('/hb4', mainController.hb4);
 router.get('/hb5', mainController.hb5);
 router.get('/page-with-image', mainController.pageWithImage);
+router.get('ui', MainController.ui);
 
 export default router;
