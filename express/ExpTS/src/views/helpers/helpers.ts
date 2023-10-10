41d1ab3943a62b1defe460c8cfd97a90c10cb630
Ícaro <icaropereirasun@gmail.com>
2023-10-09 20:35:47 -0400

    feat: finished exercise 8

--- express/ExpTS/src/views/helpers/helpers.ts
+++ express/ExpTS/src/views/helpers/helpers.ts
@@ -1,4 +1,4 @@
-import { Tech } from './helpersType';
+import { Tech, Prof } from './helpersType';
 
 export function listTechs(techs: Tech[]) {
   const list = techs
@@ -6,4 +6,10 @@ export function listTechs(techs: Tech[]) {
     .map((value) => `<li>${value.name} - ${value.type}</li>`);
 
   return list.join('');
+}
+
+export function listProfs(profs: Prof[]) {
+  const list = profs.map((value) => `<li>${value.nome} - ${value.sala}</li>`);
+
+  return `<ul>${list.join('')}</ul>`;
 }
