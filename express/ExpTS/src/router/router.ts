b97394e46f2ccc57295550d1a4bb1957a5bd6a74
Ícaro <icaropereirasun@gmail.com>
2023-10-09 20:28:11 -0400

    feat: add router hb5

--- express/ExpTS/src/router/router.ts
+++ express/ExpTS/src/router/router.ts
@@ -74,4 +74,15 @@ router.get('/hb4', (req: Request, res: Response) => {
   res.render('hb4', { technologies, layout: false });
 });
 
+router.get('/hb5', (req: Request, res: Response) => {
+  const profes = [
+    { nome: 'David Fernandes', sala: 1238 },
+    { nome: 'Horácio Fernandes', sala: 1233 },
+    { nome: 'Edleno Moura', sala: 1236 },
+    { nome: 'Elaine Harada', sala: 1231 },
+  ];
+
+  res.render('hb5', { profes, layout: false });
+});
+
 export default router;
