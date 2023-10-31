a7e68319506881a0795688c141a7cb9fa993f077
Ícarosun <icaro.pereira@icomp.ufam.edu.br>
2023-10-31 18:22:03 -0400

    feature: add produto router

--- banco_dados/laboratorio_03/src/router/v1Router.ts
+++ banco_dados/laboratorio_03/src/router/v1Router.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import categoriaRouter from "../resources/categoria/categoria.router";
 import clienteRouter from "../resources/cliente/cliente.router";
+import produtoRouter from "../resources/produto/produto.router";
 
 const v1Router = Router();
 
 v1Router.use("/categoria", categoriaRouter);
 v1Router.use("/cliente", clienteRouter);
+v1Router.use("/produto", produtoRouter);
 
 export default v1Router;
