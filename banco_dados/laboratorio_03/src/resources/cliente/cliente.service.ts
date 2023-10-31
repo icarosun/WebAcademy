1e0129652bf29de2f75ede61de7dff0eacc4a59f
Ícaro <icaropereirasun@gmail.com>
2023-10-30 23:36:17 -0400

    feature: add create cliente

--- banco_dados/laboratorio_03/src/resources/cliente/cliente.service.ts
+++ banco_dados/laboratorio_03/src/resources/cliente/cliente.service.ts
@@ -1,9 +1,7 @@
-import { PrismaClient, clienteEndereco } from "@prisma/client";
+import { PrismaClient, Cliente } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function createCliente(
-  value: clienteEndereco
-): Promise<clienteEndereco> {
-  return await prisma.clienteEndereco.create({ data: value });
+export async function createCliente(value: Cliente): Promise<Cliente> {
+  return await prisma.cliente.create({ data: value });
 }
