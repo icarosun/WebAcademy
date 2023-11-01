3028ed4cd6c7df55575518910134df3ed3415e17
Ícarosun <icaro.pereira@icomp.ufam.edu.br>
2023-10-31 21:02:43 -0400

    feature: add session

--- expLoja/backend/src/index.ts
+++ expLoja/backend/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import morgan from "morgan";
 import dotenv from "dotenv";
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import { v4 as uuidv4 } from "uuid";
 
 import validateEnv from "./utils/validateEnv";
 import router from "./router";
-import cookieParser from "cookie-parser";
 import setLangCookie from "./middleware/setLangCookie";
 
 dotenv.config();
@@ -16,6 +18,15 @@ const PORT = process.env.PORT ?? 7777;
 app.use(morgan("combined"));
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  session({
+    genid: () => uuidv4(),
+    secret: "JOSJFJFljfalsjfoqJFLAJFDLAjfascpoiqwerqj",
+    resave: true,
+    cookie: { maxAge: 10 * 24 * 60 * 60 * 1000 },
+    saveUninitialized: true,
+  })
+);
 app.use(setLangCookie);
 app.use(router);
 
