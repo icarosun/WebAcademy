f03cf48e9700eb260e1a402cef8a52deb23097a8
Ícarosun <icaro.pereira@icomp.ufam.edu.br>
2023-11-07 18:08:59 -0400

    create table tiposUsuarios and table usuario

---
+++ expLoja/backend/prisma/seeds.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+import { TiposUsuarios } from "../src/resources/tipoUsuario/tipoUsuario.constants";
+
+const prisma = new PrismaClient();
+
+async function tipoUsuarioSeeds() {
+  await prisma.tipoUsuario.createMany({
+    data: [
+      { id: TiposUsuarios.CLIENT, rotulo: "cliente" },
+      { id: TiposUsuarios.ADMIN, rotulo: "admin" },
+    ],
+    skipDuplicates: true,
+  });
+}
+
+tipoUsuarioSeeds()
+  .then(() => {
+    console.log("Registros inseridos com sucesso!");
+    prisma.$disconnect();
+  })
+  .catch((err) => {
+    console.log(err);
+  });
---
+++ expLoja/backend/src/resources/tipoUsuario/tipoUsuario.constants.ts
@@ -0,0 +1,4 @@
+export enum TiposUsuarios {
+  ADMIN = "2272583b-f5a5-4238-b68f-44061fe0016b",
+  CLIENT = "380c7fd1-f5f8-490a-8fc4-d4e415c78d96",
+}
