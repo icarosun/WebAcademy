f927f20d23f02129547f658921e6a28c6dc325de
Ícaro <icaropereirasun@gmail.com>
2023-11-13 07:39:05 -0400

    feature: add heading and ListaTask

--- react/Atividade_1/src/App.tsx
+++ react/Atividade_1/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ListaToDo from './componentes/ListaToDo';
 
 function App() {
   return (
     <>
       <h1>Try not, Do or Do not!</h1>
       
+      <ListaToDo />
     </>
   )
 }
