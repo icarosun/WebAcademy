0955b1eda7e82cdd9df58bc03ac323e04a9b41c3
Ícaro <icaropereirasun@gmail.com>
2023-11-18 15:46:58 -0400

    feature: create component List Movie

---
+++ react/Atividade_3/src/componentes/ListCategoryMovie/index.tsx
@@ -0,0 +1,34 @@
+import { Row, Col } from "react-bootstrap";
+import { Movie, TheMovieDB } from "../../services/movie.service";
+
+interface ListCategoryMovieProps {
+  category: string;
+  movies: TheMovieDB | undefined; 
+  onMoreInfoMovie: (obj: Movie) => void; 
+}
+
+export default function ListCategoryMovie(props: ListCategoryMovieProps) {
+  return (
+    <>
+      <h2 style = {{textAlign: "left"}} >{props.category}</h2>
+
+      <Row>
+        {props.movies?.results.map((movie) => {
+          return (
+            <Col key={movie.id} onClick={() => props.onMoreInfoMovie(movie)}>
+              <img
+                style={{ width: 200, borderRadius: 7,}}
+                src={`${import.meta.env.VITE_APP_BASE_URL_IMAGEM}/${
+                  movie.poster_path
+                }`}
+              />
+              <h5>
+                {movie.original_title}
+              </h5>
+            </Col>
+          );
+        })}
+      </Row>
+    </>
+  );
+}
