b3912de7cba9aa8495f9055ece01fd5de9361d3c
Ícaro <icaropereirasun@gmail.com>
2023-11-18 15:26:01 -0400

    fix: rename props

--- react/Atividade_3/src/componentes/ModalInfoMovie/index.tsx
+++ react/Atividade_3/src/componentes/ModalInfoMovie/index.tsx
@@ -7,14 +7,14 @@ import { MovieDetails } from "../../services/movie.details.service";
 interface ModalInfoMovieProps {
   isShow: boolean;
   onClose: () => void;
-  movieDetail: MovieDetails | undefined;
+  movieDetails: MovieDetails | undefined;
 }
 
 export default function ModalInfoMovie(props: ModalInfoMovieProps) {
   return(
     <Modal show = {props.isShow} onHide = {props.onClose} size = "xl" centered>
       <Modal.Header closeButton>
-        <Modal.Title>{props.movieDetail?.title} </Modal.Title>  
+        <Modal.Title>{props.movieDetails?.title} </Modal.Title>  
       </Modal.Header>
       <Modal.Body>
         <Row>
@@ -22,47 +22,47 @@ export default function ModalInfoMovie(props: ModalInfoMovieProps) {
             <img
               style={{ width: 200, borderRadius: 7,}}
               src={`${import.meta.env.VITE_APP_BASE_URL_IMAGEM}/${
-              props.movieDetail?.poster_path}`}
+              props.movieDetails?.poster_path}`}
             />
-            <p>{props.movieDetail?.release_date}<FontAwesomeIcon className="pt-2 px-2" icon={faCircle} size = "2xs"/>
-              {props.movieDetail?.runtime}m
+            <p>{props.movieDetails?.release_date}<FontAwesomeIcon className="pt-2 px-2" icon={faCircle} size = "2xs"/>
+              {props.movieDetails?.runtime}m
             </p>
             <h6>Orçamento</h6>
-            <p>{props.movieDetail?.revenue}</p>
+            <p>{props.movieDetails?.revenue}</p>
               
             <Badge bg="warning" text="dark">
-              <Alert.Link target = "_blank" href={`https://www.imdb.com/title/${props.movieDetail?.imdb_id}`}><span className="fw-bold" >IMDb</span> | {props.movieDetail?.imdb_id}</Alert.Link>
+              <Alert.Link target = "_blank" href={`https://www.imdb.com/title/${props.movieDetails?.imdb_id}`}><span className="fw-bold" >IMDb</span> | {props.movieDetails?.imdb_id}</Alert.Link>
             </Badge>
           </Col>
-          <Col>
+          <Col >
             <Row xs = "auto" className = "justify-content-around">
               <Card border = "success" className = "text-center">
                 <Card.Body>
                   <Card.Title>Média de avaliação</Card.Title>
-                  <Card.Text >{props.movieDetail?.vote_average}</Card.Text>
+                  <Card.Text >{props.movieDetails?.vote_average}</Card.Text>
                 </Card.Body>
               </Card>
               <Card border = "primary" className = "text-center">
                 <Card.Body>
                   <Card.Title>Número de avaliações</Card.Title>
-                  <Card.Text>{props.movieDetail?.vote_count}</Card.Text>
+                  <Card.Text>{props.movieDetails?.vote_count}</Card.Text>
                 </Card.Body>
               </Card>
               <Card border = "primary border-success-subtle" className = "text-center">
                 <Card.Body>
                   <Card.Title>Popularidade</Card.Title>
-                  <Card.Text  >{props.movieDetail?.popularity}</Card.Text>
+                  <Card.Text  >{props.movieDetails?.popularity}</Card.Text>
                 </Card.Body>
               </Card>
             </Row>
             <Stack direction = "horizontal" gap={3} className="my-3">
-              {props.movieDetail?.genres.map((genre) => {
-                return (<Badge bg = "dark">{genre.name}</Badge>);
+              {props.movieDetails?.genres.map((genre) => {
+                return (<Badge bg = "dark" >{genre.name}</Badge>);
               })}
             </Stack>
-            <p className="fst-italic">{props.movieDetail?.tagline}</p>
+            <p className="fst-italic">{props.movieDetails?.tagline}</p>
             <h5>Sinopse</h5>
-            <p>{props.movieDetail?.overview}</p>
+            <p>{props.movieDetails?.overview}</p>
           </Col>
         </Row>
       </Modal.Body>
