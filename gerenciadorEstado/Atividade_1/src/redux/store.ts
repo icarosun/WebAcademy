3ffeb30b6a98d0fa39a00c39b720625db8b57c1c
Ícaro <icaropereirasun@gmail.com>
2023-11-19 23:01:27 -0400

    feat: add redux-persist

--- gerenciadorEstado/Atividade_1/src/redux/store.ts
+++ gerenciadorEstado/Atividade_1/src/redux/store.ts
@@ -1,13 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
-import CaixaReducer from "./slices/caixa.slice";
+import storageSession from "redux-persist/lib/storage/session";
 import TaskReducer from "./slices/task.slice";
+import { combineReducers } from "redux";
+import { persistReducer, persistStore } from "redux-persist";
+
+const rootReducer = combineReducers({
+  task: TaskReducer,
+})
+
+
+const persistConfig = {
+  key: 'root',
+  storage: storageSession,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    caixa: CaixaReducer,
-    task: TaskReducer 
-  }
+  reducer: persistedReducer,
 });
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store);
+
