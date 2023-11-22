300084d73edc8eaf5cc7a31005d61cc400e695ec
Ícaro <icaropereirasun@gmail.com>
2023-11-22 14:26:26 -0400

    change component from carousel

--- react/Atividade_3/src/componentes/ListCategoryMovie/index.tsx
+++ react/Atividade_3/src/componentes/ListCategoryMovie/index.tsx
@@ -4,13 +4,35 @@ import Slider from "react-slick";
 import { Movie, TheMovieDB } from "../../services/movie.service";
 import "../../slick.css"; 
 import "../../slick-theme.css"
-
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquareCaretLeft, faSquareCaretRight  } from "@fortawesome/free-solid-svg-icons";
 
 interface ListCategoryMovieProps {
   category: string;
   movies: TheMovieDB | undefined;
   onMoreInfoMovie: (obj: Movie) => void;
+}
+
+function NextArrow(props: any) {
+  const {className, style, onClick } = props;
+  return (
+    <FontAwesomeIcon className = {className}
+      style = {{...style, display: "block"}} 
+      onClick = {onClick}
+      icon={faSquareCaretRight}
+    />
+  );
+}
+
+function PrevArrow(props: any) {
+  const {className, style, onClick } = props;
+  return (
+    <FontAwesomeIcon className = {className}
+      style = {{...style, display: "block"}} 
+      onClick = {onClick}
+      icon={faSquareCaretLeft}
+    />
+  );
 }
 
 export default function ListCategoryMovie(props: ListCategoryMovieProps) {
@@ -20,23 +42,25 @@ export default function ListCategoryMovie(props: ListCategoryMovieProps) {
     speed: 500,
     slidesToShow: 5,
     slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />
   }
 
   return (
     <>
-      <h4 style={{ textAlign: "left" }}>{props.category}</h4>
+      <h4 className = "mt-1" style={{ textAlign: "left" }}>{props.category}</h4>
       
       <Slider {...settings}> 
         {props.movies?.results.map((movie) => {
           return (
-            <Card className = "justify-content-center" key={movie.id} onClick={() => props.onMoreInfoMovie(movie)}>
+            <div className = "justify-content-center p-1" key={movie.id} onClick={() => props.onMoreInfoMovie(movie)}>
               <Card.Img variant = "top"
                 src={`${import.meta.env.VITE_APP_BASE_URL_IMAGEM}/${
                   movie.poster_path
                 }`}
+                alt = {movie.original_title}
               />
-              <h5>{movie.original_title}</h5>
-            </Card>
+            </div>
           );
         })}
       </Slider>
