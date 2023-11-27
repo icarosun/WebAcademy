50b32bfa44ceeb6ba25704255ddae3c37fb62319
Ícaro <icaropereirasun@gmail.com>
2023-11-27 17:02:13 -0400

    Initial commit

---
+++ teste/myapp_test/src/utils/tests/validations.test.js
@@ -0,0 +1,27 @@
+const { firstName } = require("../validations.js");
+
+describe("firstName()", () => {
+  it("should return the first name when the full name is given", () => {
+    const fullName = "John Doe Etc";
+    const result = firstName(fullName);
+    expect(result).toBe("Joen");
+  });
+
+  it("should return the same name when no blank space is found", () => {
+    const name = "Alice";
+    const result = firstName(name);
+    expect(result).toBe(name);
+  });
+
+  it("should return the first name correctly when theres blank space in the start", () => {
+    const name = " Alice Test";
+    const result = firstName(name);
+    expect(result).toBe("Alice");
+  });
+
+  it("should return the first name correctly when theres blank space in the end", () => {
+    const name = "Alice Test ";
+    const result = firstName(name);
+    expect(result).toBe("Alice");
+  })
+});
